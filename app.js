529bd1f442596a13638bc35aff7a3a0cbaab6f8b
luciD <sosalnet@yandex.ru>
2023-11-20 21:41:58 +0300

    refactored mailer

--- app.js
+++ app.js
@@ -21,18 +21,6 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'timur2003',
-//   database: 'coffee',
-// });
-
-// connection.connect((err) => {
-//   if (err) throw err;
-//   console.log('Connected to the database');
-// });
-
 app.use(bodyParser.json());
 
 app.use(cors(
@@ -72,7 +60,7 @@ app.use(productRoutes);
 
 app.use(userRoutes);
 
-app.use('/send-email', mailerRoutes);
+app.use(mailerRoutes);
 
 app.use((req, res, next) => next(new NotFoundError('Страница не найдена')));
 app.use(errorLogger);
--- models/mailer.js
+++ models/mailer.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
+// mailer.js
 const nodemailer = require('nodemailer');
 
-const sendEmail = async (req, res, next) => {
+const sendEmail = async ({ from, subject, text }) => {
   try {
-    const { from, subject, text } = req.body;
-
     // Создание транспорта для отправки писем
     const transporter = nodemailer.createTransport({
-      // host: '31.31.196.190',
       host: process.env.HOST,
       port: 587,
       secure: false,
@@ -21,14 +19,15 @@ const sendEmail = async (req, res, next) => {
     transporter.verify(function (error, success) {
       if (error) {
         console.error('Ошибка проверки подключения: ', error);
-        res.status(500).json({ success: false, message: 'Ошибка при проверке подключения' });
+        // Вызываем ошибку, чтобы ее обработать в блоке catch
+        throw new Error('Ошибка при проверке подключения');
       } else {
         console.log('Соединение установлено успешно');
 
         // Настройка письма
         const mailOptions = {
           from: from,
-          to: 'your_email@example.com',
+          to: process.env.MAIL,
           subject: subject,
           text: text,
         };
@@ -37,17 +36,18 @@ const sendEmail = async (req, res, next) => {
         transporter.sendMail(mailOptions, function (error, info) {
           if (error) {
             console.error('Ошибка отправки письма: ', error);
-            res.status(500).json({ success: false, message: 'Произошла ошибка при отправке письма' });
+            // Вызываем ошибку, чтобы ее обработать в блоке catch
+            throw new Error('Произошла ошибка при отправке письма');
           } else {
             console.log('Письмо отправлено: ', info.messageId);
-            res.json({ success: true, message: 'Письмо успешно отправлено' });
           }
         });
       }
     });
   } catch (error) {
     console.error('Общая ошибка: ', error);
-    res.status(500).json({ success: false, message: 'Произошла общая ошибка' });
+    // Вызываем ошибку, чтобы ее обработать в блоке catch
+    throw new Error('Произошла общая ошибка');
   }
 };
 
