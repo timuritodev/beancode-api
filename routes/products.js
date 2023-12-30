22b58abb19f8dbf46051ae7958e863dcf1632f9e
luciD <sosalnet@yandex.ru>
2023-12-30 03:39:01 +0300

    minor improvements in addToCart

--- routes/products.js
+++ routes/products.js
@@ -33,13 +33,14 @@ router.get('/products/:id', async (req, res) => {
 
 router.post('/cart/add', async (req, res) => {
   const { userId, productId } = req.body;
-
+  const product = await productModel.getProductById(productId);
   try {
     // Вызываем метод модели корзины для добавления товара
     const result = await productModel.addToCart(userId, productId);
 
     if (result.success) {
-      res.json({ message: 'Product added to cart successfully' });
+      res.json({ product });
+      res.status(201)
     } else {
       res.status(400).json({ error: 'Failed to add product to cart' });
     }
