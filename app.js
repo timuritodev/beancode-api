5ea7eea02d7dfa47243d2d08546db92891225e4b
luciD <sosalnet@yandex.ru>
2024-02-20 14:42:21 +0300

    updated cors

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ app.use(
   })
 );
 
-// app.use((req, res, next) => {
-//   res.header({ "Access-Control-Allow-Origin": "*" });
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header({ "Access-Control-Allow-Origin": "*" });
+  next();
+});
 
 // const config = dotenv.config({
 //   path: path
