a0f568a0c03b48f262723fe5b63f40ba95a74d76
luciD <sosalnet@yandex.ru>
2024-03-04 23:43:54 +0300

    modified mailer and added some images

--- models/mailer.js
+++ models/mailer.js
@@ -23,56 +23,50 @@ const sendPasswordResetEmail = async (email, token) => {
   await transporter.sendMail(mailOptions);
 };
 
-const sendEmail = async ({ from, subject, text }) => {
-  try {
-    // Создание транспорта для отправки писем
-    const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.MAIL,
-        pass: process.env.PASS,
-      },
-    });
+const sendEmailInfo = async ({ email, subject, text }) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.PASS,
+    },
+  });
 
-    // Проверка подключения
-    transporter.verify(function (error, success) {
-      if (error) {
-        console.error("Ошибка проверки подключения: ", error);
-        // Вызываем ошибку, чтобы ее обработать в блоке catch
-        throw new Error("Ошибка при проверке подключения");
-      } else {
-        console.log("Соединение установлено успешно");
+  const mailOptions = {
+    from: email,
+    to: process.env.MAIL,
+    subject: subject,
+    text: text,
+  };
 
-        // Настройка письма
-        const mailOptions = {
-          from: from,
-          to: process.env.MAIL,
-          subject: subject,
-          text: text,
-        };
+  await transporter.sendMail(mailOptions);
+};
 
-        // Отправка письма
-        transporter.sendMail(mailOptions, function (error, info) {
-          if (error) {
-            console.error("Ошибка отправки письма: ", error);
-            // Вызываем ошибку, чтобы ее обработать в блоке catch
-            throw new Error("Произошла ошибка при отправке письма");
-          } else {
-            console.log("Письмо отправлено: ", info.messageId);
-          }
-        });
-      }
-    });
-  } catch (error) {
-    console.error("Общая ошибка: ", error);
-    // Вызываем ошибку, чтобы ее обработать в блоке catch
-    throw new Error("Произошла общая ошибка");
-  }
+const sendOrderDetails = async ({ email, greetings }) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.MAIL,
+    to: email,
+    subject: "Детали заказа",
+    text: greetings,
+  };
+
+  await transporter.sendMail(mailOptions);
 };
 
 module.exports = {
-  sendEmail,
+  sendEmailInfo,
   sendPasswordResetEmail,
+  sendOrderDetails,
 };
