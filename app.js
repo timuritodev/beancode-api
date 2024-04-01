351a1b5f0b96fe8182ed25c898777400ba3a2781
luciD <sosalnet@yandex.ru>
2024-04-02 00:34:50 +0300

    cookie fixes 6

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(
     saveUninitialized: true,
     store: sessionStore,
     cookie: {
-      secure: false,
+      secure: true,
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
       sameSite: 'None',
