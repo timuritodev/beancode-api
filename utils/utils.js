b8dd5bc0e739b92f4671bf7fa8ad375852be5551
luciD <sosalnet@yandex.ru>
2026-01-04 18:32:12 +0300

    small fix 2.0

--- utils/utils.js
+++ utils/utils.js
@@ -1,29 +1,31 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-const urlRegex = /^https?:\/\/(www\.)?[a-zA-Z\0-9]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
+const urlRegex =
+	/^https?:\/\/(www\.)?[a-zA-Z\0-9]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
 
 const emailRegex = /^[a-z0-9-]+@[a-z0-9-.]+/i;
 
 const allowedCors = [
-  'https://my-domain.com',
-  'http://my-domain.com',
-  'localhost:3000',
-  'http://beancode.ru',
+	'https://my-domain.com',
+	'http://my-domain.com',
+	'localhost:3000',
+	'http://beancode.ru',
 ];
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'timur2003',
-  database: 'coffee',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0,
 });
 
 module.exports = {
-  emailRegex,
-  urlRegex,
-  allowedCors,
-  pool,
+	emailRegex,
+	urlRegex,
+	allowedCors,
+	pool,
 };
